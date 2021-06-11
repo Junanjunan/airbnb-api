0f679b4202c08f3212d726c510413f4eeb5c2a7d
unknown <jjj1305@hanmail.net>
2021-06-12 00:24:05 +0900

    a

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 app_name = "rooms"
 
-urlpatterns = [path("list/", views.list_rooms)]
+
+urlpatterns = [
+    path("", views.rooms_view),
+    path("<int:pk>/", views.SeeRoomView.as_view()),
+]
