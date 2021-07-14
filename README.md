66f14ded729357280ed53120994d14ca79633cb3
unknown <jjj1305@hanmail.net>
2021-07-14 12:20:21 +0900

    lecture 2.12

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ REST & GraphQL API of the Airbnb Clone using Django REST Framework and Graphene 
 
 ### API Actions
 
-- [ ] JWT Authentication
+- [X] JWT Authentication
 - [X] Login (JWT)
 - [X] Create Account
 - [X] See Profile
--- users/views.py
+++ users/views.py
@@ -95,7 +95,7 @@ def login(request):
         return Response(status=status.HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if user is not None:
-        encoded_jwt = jwt.encode({"id": user.pk}, settings.SECRET_KEY, algorithm="HS256")   # encoded_jwt = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
+        encoded_jwt = jwt.encode({"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256")   # encoded_jwt = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
         print(encoded_jwt)
         return Response(data={"token": encoded_jwt})
     else:
